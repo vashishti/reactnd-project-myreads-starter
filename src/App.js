6035a06e551ca59cf9b2bff623d01bc056c501f0
Vashishti <szg0081@auburn.edu>
2018-01-21 15:20:14 -0600

    style: Import API File

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
+  // state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
-  }
+    // showSearchPage: false
+  // }
 
+  state = {
+    contacts: []
+  }
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts })
+    })
+  }
   render() {
     return (
       <div className="app">
